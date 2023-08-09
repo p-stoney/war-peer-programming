e1220b0cc3d283fe176069fded07b4a07214bde4
p-stoney <137152792+p-stoney@users.noreply.github.com>
2023-08-09 17:42:53 +0000

    Wed, Aug 9, 2023, 1:42 PM -04:00

--- wargame.py
+++ wargame.py
@@ -2,3 +2,12 @@ class Player
     def __init__(self, name):
         self.name = name
         self.hand = []
+
+    def draw_card(self):
+        ...
+
+    def take_card(self, cards)
+        ...
+
+    def has_cards(self):
+        ...
