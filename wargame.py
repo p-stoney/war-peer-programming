4dfa095ca1e6ee052a176f169c37a995643ebaa6
p-stoney <137152792+p-stoney@users.noreply.github.com>
2023-08-09 17:45:50 +0000

    Wed, Aug 9, 2023, 1:45 PM -04:00

--- wargame.py
+++ wargame.py
@@ -1,13 +1,18 @@
 class Player
     def __init__(self, name):
+        # str representing the player's name
         self.name = name
+        # list to hold card objects in the player's hand
         self.hand = []
 
     def draw_card(self):
+        # removes or returns top card from the player's hand
         ...
 
     def take_card(self, cards)
+        # adds one(/more) won cards to the bottom of the player's hand
         ...
 
     def has_cards(self):
+        # returns true if the player has any remaining cards, false otherwise
         ...
